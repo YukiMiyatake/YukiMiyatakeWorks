22820b2ffb209613f88dc04eb649fe0438baa592
YukiMiyatake <YukiMariaMiyatake@gmail.com>
2018-08-30 08:25:45 +0900

    ハッチングの追加

---
+++ UnityViewer/Assets/scripts/PostProcess1.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PostProcess1 : MonoBehaviour {
+    public Material mat;
+    void OnRenderImage(RenderTexture src, RenderTexture dest) {
+        Graphics.Blit(src, dest, mat);
+    }
+}
