e071c46301bffd2571d2c7601d4ade26ade6f751
YukiMiyatake <yukimariamiyatake@gmail.com>
2018-10-20 03:21:41 +0900

    DynamicResolution

---
+++ DynamicResolutionSample/Assets/DebugText.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using UnityEngine.UI;
+
+public class DebugText : MonoBehaviour
+{
+    private Text text_;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text_ = GetComponent<Text>();
+
+    }
+
+// Update is called once per frame
+void Update()
+    {
+        var frameTimings = new FrameTiming[1];
+        FrameTimingManager.CaptureFrameTimings();
+        uint numReturened = FrameTimingManager.GetLatestTimings(1, frameTimings);
+        var ft = frameTimings[0];
+
+        text_.text = string.Format(  "Screen  Width:{0,-4} Height:{1,-4}\n" 
+                                   + "cpuFrameTime  {2}\n"
+                                   + "gpuFrameTime  {3}\n"
+                                   + "cpuTiminePresentCalled  {4}\n"
+                                   + "cpuTimeFrameComplete  {5}\n"
+                                   + "heightScale  {6}\n"
+                                   + "widthScale  {7}\n"
+                                   + "syncInterval  {8}\n"
+                                   , Screen.width, Screen.height
+                                   , ft.cpuFrameTime, ft.gpuFrameTime, ft.cpuTimePresentCalled, ft.cpuTimeFrameComplete
+                                   , ft.heightScale, ft.widthScale, ft.syncInterval);
+
+
+    }
+}
---
+++ DynamicResolutionSample/Assets/Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        var frameTimings = new FrameTiming[1];
+        FrameTimingManager.CaptureFrameTimings();
+        uint numReturened = FrameTimingManager.GetLatestTimings(1, frameTimings);
+        var ft = frameTimings[0];
+
+
+        Debug.Log( "CPU" + frameTimings[0].cpuFrameTime);
+        */
+    }
+}
