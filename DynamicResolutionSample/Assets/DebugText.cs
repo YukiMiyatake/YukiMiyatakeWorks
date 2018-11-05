71ecb78c4142e95c0c2f8c0b22a46e9a3b2841e6
YukiMiyatake <yukimariamiyatake@gmail.com>
2018-11-06 03:14:06 +0900

    Refactor

--- DynamicResolutionSample/Assets/DebugText.cs
+++ DynamicResolutionSample/Assets/DebugText.cs
@@ -7,24 +7,28 @@ using UnityEngine.UI;
 public class DebugText : MonoBehaviour
 {
     private Text text_;
+    public FrameTiming ft;
 
     // Start is called before the first frame update
     void Start()
     {
         text_ = GetComponent<Text>();
+ //       frameTimings = new FrameTiming[2];
 
     }
 
 // Update is called once per frame
 void Update()
     {
-        var frameTimings = new FrameTiming[1];
+/*
+        var frameTimings = new FrameTiming[2];
         FrameTimingManager.CaptureFrameTimings();
-        uint numReturened = FrameTimingManager.GetLatestTimings(1, frameTimings);
-        var ft = frameTimings[0];
-
-        text_.text = string.Format(  "Screen  Width:{0,-4} Height:{1,-4}\n" 
-                                   + "cpuFrameTime  {2}\n"
+        uint numReturened = FrameTimingManager.GetLatestTimings(2, frameTimings);
+        var ft = frameTimings[1];
+*/
+        text_.text = string.Format(  "FPS {9}\n"
+                                   + "Screen  Width:{0,-4} Height:{1,-4}\n" 
+                                   + "cpuFrameTime  {2:F8}\n"
                                    + "gpuFrameTime  {3}\n"
                                    + "cpuTiminePresentCalled  {4}\n"
                                    + "cpuTimeFrameComplete  {5}\n"
@@ -33,7 +37,9 @@ void Update()
                                    + "syncInterval  {8}\n"
                                    , Screen.width, Screen.height
                                    , ft.cpuFrameTime, ft.gpuFrameTime, ft.cpuTimePresentCalled, ft.cpuTimeFrameComplete
-                                   , ft.heightScale, ft.widthScale, ft.syncInterval);
+                                   , ft.heightScale, ft.widthScale, ft.syncInterval
+                                   , 1f / Time.deltaTime
+                                   );
 
 
     }
