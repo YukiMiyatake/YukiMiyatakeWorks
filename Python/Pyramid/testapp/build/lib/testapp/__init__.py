aca2d6b60559639bcffb4a833fdcb964fb9ece99
YukiMiyatake <YukiMariaMiyatake@gmail.com>
2020-01-08 03:51:50 +0900

    add newpage and post form by Deform

---
+++ Python/Pyramid/testapp/build/lib/testapp/__init__.py
@@ -0,0 +1,15 @@
+from pyramid.config import Configurator
+
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    config = Configurator(settings=settings)
+    config.include('pyramid_jinja2')
+    config.include('.models')
+    config.include('.routes')
+    config.scan()
+    
+    config.add_jinja2_renderer(".html")
+
+    return config.make_wsgi_app()
