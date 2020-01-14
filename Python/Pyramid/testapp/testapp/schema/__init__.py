00b4854c6b319a991b86dd927d4e39b5cc30eb7c
YukiMiyatake <YukiMariaMiyatake@gmail.com>
2020-01-14 16:21:15 +0900

    do Validation

--- Python/Pyramid/testapp/testapp/schema/__init__.py
+++ Python/Pyramid/testapp/testapp/schema/__init__.py
@@ -6,20 +6,7 @@ class NewPageSchema(colander.MappingSchema):
 #class NewPageSchema(CSRFSchema):
     username = colander.SchemaNode(colander.String(), title="username",
                                    validator=colander.Length(min=4, min_err='Shorter than minimum length ${min}'),
-                                   default="", missing=colander.null)
+                                   default="")
     email = colander.SchemaNode(colander.String(), title="email",
-                                   validator=colander.Email(), missing=colander.null)
-
-    def validator(self, node: "NewPageSchema", appstruct: dict):
-        """Custom schema level validation code."""
-
-        # appstruct is Colander appstruct after all other validations have passed
-        # Note that this method might not ever be reached
-        if appstruct["username"]  == colander.null:
-            # This error message appears at the top of the form
-            raise colander.Invalid(node["username"], "username least 4 word.")
-
-        if appstruct["email"] == colander.null:
-            # This error message appears at the top of the form
-            raise colander.Invalid(node["email"], "Please fill in email field if you want to send a preview email.")
+                                   validator=colander.Email())
 
--- Python/Pyramid/testapp/testapp/views/default.py
+++ Python/Pyramid/testapp/testapp/views/default.py
@@ -28,24 +28,29 @@ class Views(object):
     def form_(self):
         schema = NewPageSchema()
         btn = deform.form.Button(name="newpage", title="newpage")
-        return deform.form.Form(schema, buttons=(btn,), action="/newpage")
+        return deform.form.Form(schema, buttons=(btn,), action="/")
 
     @view_config(route_name='home', renderer='../templates/mytemplate.html')
     def my_view(self):
-        form = self.form_.render()
-        return {"rendered_form": form}
-
-    @view_config(route_name="newpage", renderer="../templates/newpage.html")
-    def newpage(self):
+        form = {}
         if 'newpage' in self.request.params:
             controls = self.request.POST.items()
 
             try:
                 self.form_.validate(controls)
+                self.request.session["testapp.username"] = self.request.params.get("username")
+                self.request.session["testapp.email"] = self.request.params.get("email")
+                return HTTPFound(location= self.request.route_url('newpage'))
             except ValidationFailure as e:
-                return HTTPFound(location='/')
+                form=e.render()
+        else:
+            form = self.form_.render()
 
+        return {"rendered_form": form}
 
-        username = self.request.params.get("username")
-        email = self.request.params.get("email")
+    @view_config(route_name="newpage", renderer="../templates/newpage.html")
+    def newpage(self):
+
+        username = self.request.session["testapp.username"]
+        email = self.request.session["testapp.email"]
         return {"username": username, "email": email }
